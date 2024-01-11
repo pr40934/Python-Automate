4c804dab4c9a11cd46603530fded82fe19bb946c
pr40934 <pr4093403@gmail.com>
2024-01-11 12:30:17 -0500

    Day_one

---
+++ Day One.py
@@ -0,0 +1,18 @@
+# 11 - 01 - 2024 10:26
+
+# Python 
+# Python Book, Automate The Borning Stuff With Python 
+
+5 ** 5 # Exponent  
+
+print('Exponent :: ',2 ** 5) # 2 X 2 X 2 X 2 X 2 = 32
+
+5 % 5 # Modulus Or Remainder
+
+                    # Divsor --> 8 # Dividend --> 22    
+print("Modulus",22 % 8) # 8 ) 22 ( 2 --> Quotient
+                        #     16
+                        #------------
+                        #      6  --> Remainder
+
+                        
