8c3374a42e5698f81b96caa36f2839a471e4cfa4
pr40934 <pr4093403@gmail.com>
2024-01-12 11:09:45 -0500

    operators&DataTypes&Variables

---
+++ Data Types.py
@@ -0,0 +1,26 @@
+# Integer Data Type 
+
+intDataTypeVarabile = 5,575,-528,547,632
+
+floatDataTypeVarabile = -1.253,2.56,485.23
+
+stringDataTypeVarabile = 'string','text','names','emails'
+
+# string Concatenation 
+
+firstName = "Pratap "
+
+lastName  = "Raju"
+
+fullName = firstName + lastName
+
+print("fullName ",fullName)
+
+# Can not add with Number
+# but we can Multipl with number
+
+userName = "Pratap "
+
+mult = userName * 5
+
+print(' Multiplcation ',mult)
---
+++ Operators.py
@@ -0,0 +1,41 @@
+# 11 - 01 - 2024 10:26 Day One
+
+# Python 
+# Python Book, Automate The Borning Stuff With Python 
+
+# ======================================================= #
+# Operators
+
+5 ** 5 # Exponent  
+
+print('Exponent :: ',2 ** 5) # 2 X 2 X 2 X 2 X 2 = 32
+
+5 % 5 # Modulus Or Remainder
+
+                    # Divsor --> 8 # Dividend --> 22    
+print("Modulus",22 % 8) # 8 ) 22 ( 2 --> Quotient
+                        #     16
+                        #------------
+                        #      6  --> Remainder
+
+                        
+# 12 - 01 - 2024  09:15 Day Two
+
+22 / 8 # Division
+
+print('Division',22/8)  # 8 ) 22 ( 2 --> Out put for this Division
+                        #     16
+                        #------------
+                        #     6  --> Remainder
+
+3 * 5 # Multiplication
+
+print('Multiplication',3 * 5) # 3 X 5 = 15
+
+5 - 2 # Subtraction 
+
+print('Subtraction',5 - 2) # 5 - 2 = 3
+
+2 + 2 # Addititon 
+
+print('Addition',2 + 2) # 2 + 2 = 4
