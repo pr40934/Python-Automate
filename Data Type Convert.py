e21909930769c2f1878aaed376545138f3aaab7e
pr40934 <pr4093403@gmail.com>
2024-01-13 04:14:13 -0500

    datatypes&FirstProgram

---
+++ Data Type Convert.py
@@ -0,0 +1,9 @@
+floatVariable = 25.236
+print(int(floatVariable))
+
+intVariable = 52
+print(float(intVariable))
+
+stringVariable = 10
+print(str(stringVariable))
+
---
+++ first Program.py
@@ -0,0 +1,18 @@
+# 13 - 01 - 2024
+
+print('Hello World')
+
+userName = input('Enter Your Name :: ')
+
+print('Hello ' + userName)
+
+# convert String to Int
+
+userAge = input('Enter Your Age :: ')
+
+print('My name is ' + userName + ' and I am '+ str(int(userAge)+1) )
+
+
+# User name Number of characters
+
+print('Your Number of Characters ',len(userName))
