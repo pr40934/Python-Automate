4535ac6ba3fe62f0b9b31b1e5e768b7e60975516
pr40934 <pr4093403@gmail.com>
2024-01-18 10:31:54 -0500

    parcticeQuestions

--- ExceptionsHandling.py
+++ ExceptionsHandling.py
@@ -11,3 +11,40 @@ print(spam(2))
 print(spam(12))
 print('ZERO :: ',spam(0))
 print(spam(1))
+
+# 18 - 01 - 2024
+
+# 1. Why are functions advantageous to have in your programs?
+
+# Preventing the duplicate code & re Usebullity
+
+# 2. When does the code in a function execute: when the function is
+# defined or when the function is called?
+
+def callAFunction():
+    print('Who"s that')
+
+callAFunction()
+
+# 3. What statement creates a function?
+
+def statementfun():
+    pass
+
+# 4. What is the difference between a function and a function call?
+
+def createFunc():
+    print('I Created a Function')
+
+createFunc() # Calling function to Excuite 
+
+# 5. How many global scopes are there in a Python program? How many
+# local scopes?
+
+# one 
+
+# 6. What happens to variables in a local scope when the function call returns?
+
+# they are become empty
+
+# 7. 
