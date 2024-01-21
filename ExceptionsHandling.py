14507d5ecfa2a63340a03cec76a843ab672dd2c0
pr40934 <pr4093403@gmail.com>
2024-01-20 23:31:25 -0500

    Question&List

--- ExceptionsHandling.py
+++ ExceptionsHandling.py
@@ -110,4 +110,50 @@ errorHandling()
 
 # try clause  try to run code in a try clause block in any case it get any error it run except code block
  
- # 20 - 01 - 2024
+# 20 - 01 - 2024
+
+# 21 - 01 - 2024
+
+# Practice Projects
+
+# The Collatz Sequence
+
+# Write a function named collatz() that has one parameter named number. If
+# number is even, then collatz() should print number // 2 and return this value.
+# If number is odd, then collatz() should print and return 3 * number + 1.
+# Then write a program that lets the user type in an integer and that keeps
+# calling collatz() on that number until the function returns the value 1.
+# (Amazingly enough, this sequence actually works for any integer—sooner
+# or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t
+# sure why. Your program is exploring what’s called the Collatz sequence, some-
+# times called “the simplest impossible math problem.”)
+# Remember to convert the return value from input() to an integer with
+# the int() function; otherwise, it will be a string value.
+# Hint: An integer number is even if number % 2 == 0, and it’s odd if
+# number % 2 == 1.
+
+def userInput():
+    try:
+
+        userValue = int(input('Enter Number :: '))
+        calculateFunction(userValue)
+
+    except:
+        print('Exception in Input')
+
+
+def calculateFunction(userValue):
+
+    if userValue % 2 == 0:
+        finallValue = userValue // 2
+        print("finallValue : ",finallValue)
+        
+    else:
+        finallValue = 3 * userValue + 1
+
+        print("finallValue : ",finallValue)
+
+    if finallValue != 1:
+            userInput()
+    
+userInput()
---
+++ Lists.py
@@ -0,0 +1,29 @@
+# LISTS
+
+# The List Data Type
+
+x = ['car','bus','bike',1,5,None,True,False]
+
+name = ['pratap','raju']
+
+print(name[0:7])
+
+# list Length
+
+print(len(x))
+
+# Concatenation
+
+print(x + name)
+
+print(x)
+
+# Delete Iteam in a List
+
+del x[-1]
+
+print(x)
+
+# In Operator in List
+
+print('Check :: ','car' not in x ,x.index('car'))
